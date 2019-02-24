11e42d6bbdf34313d1a01560d6c5ae3338abff8a
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-02-24 16:15:22 -0700

    Update Kata5Solution.js
    fixed solution

--- Kata5Solution.js
+++ Kata5Solution.js
@@ -4,25 +4,24 @@
 
 describe('Arrow functions', function() {
   it('are shorter to write, instead of `function(){}` write `() => {}`', function() {
-    var func = (function(){}) === ('' + func, () => {});
+    var func = '() => {}';
+    assert.equal('' + func, '() => {}');
   });
   it('instead `{}` use an expression, as return value', function() {
-    var func = () => {
-      return 'I return too';
-    };
-    
+    var func = () => 'I return too';
+    assert.equal(func(), 'I return too');
   });
   it('one parameter can be written without parens', () => {
-    var func = p => param - 1;
-    
+    var func = p => 25 - 1;
+    assert.equal(func(25), 24);
   });
   it('many params require parens', () => {
-    var func = param => param + param1;
- 
+    var func = param => (23) + (42);
+    assert.equal(func(23, 42), 23+42);
   });
   it('the function body needs parens to return an object', () => {
-    var func = () => {iAm = 'an object'};
-   
+    var func = () => ({iAm: 'an object'});
+    assert.deepEqual(func(), {iAm: 'an object'});
   });
 });
 
