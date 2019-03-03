ba7c7898681e29d67f68b70bfb9381fa28f65d84
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-03-02 18:26:26 -0700

    Create Kata17solution.js
    Solution to Kata 17 - Unicode: Strings

---
+++ Kata17solution.js
@@ -0,0 +1,47 @@
+// 17: unicode - in strings
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Unicode in strings', () => {
+  it('are prefixed with `\\u` (one backslash and u)', () => {
+    const nuclear = `\u2622`;
+    assert.equal(nuclear, '☢');
+  });
+  it('value is 4 bytes/digits', () => {
+    const nuclear = '\u2622';
+    assert.equal(`no more ${nuclear}`, 'no more ☢');
+  });
+  it('even "normal" character`s values can be written as hexadecimal unicode', () => {
+    const nuclear = `\u006E\u006F more \u2622`;
+    assert.equal(nuclear, 'no more ☢');
+  });
+  it('curly braces may surround the value', () => {
+    const nuclear = `\u006E\u006F more \u2622`;
+    assert.equal(nuclear, 'no more ☢');
+  });
+});
+
+/// BROKEN CODE BELOW
+
+// 17: unicode - in strings
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Unicode in strings', () => {
+  it('are prefixed with `\\u` (one backslash and u)', () => {
+    const nuclear = 2622;
+    assert.equal(nuclear, '☢');
+  });
+  it('value is 4 bytes/digits', () => {
+    const nuclear = '\u26222';
+    assert.equal(`no more ${nuclear}`, 'no more ☢');
+  });
+  it('even "normal" character`s values can be written as hexadecimal unicode', () => {
+    const nuclear = `\u006B\u006A more \u2622`;
+    assert.equal(nuclear, 'no more ☢');
+  });
+  it('curly braces may surround the value', () => {
+    const nuclear = `\u{0000000006E}\u00006F more \u2622`;
+    assert.equal(nuclear, 'no more ☢');
+  });
+});
