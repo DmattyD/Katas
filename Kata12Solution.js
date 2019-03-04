0d3195ea60abef26d56d94883f938bda665f6c21
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-03-04 16:57:20 -0700

    Update Kata12Solution.js

--- Kata12Solution.js
+++ Kata12Solution.js
@@ -4,31 +4,31 @@
 
 describe('Destructure objects', () => {
   it('by surrounding the left-hand variable with `{}`', () => {
-    const x = 1;
+    const {x} = {x: 1}; /// added {} around const
     assert.equal(x, 1);
   });
   describe('nested', () => {
     it('multiple objects', () => {
       const magic = {first: 23, second: 42};
-      const {first, second} = magic;
+      const {first, second} = magic; /// restuructred const object to give variable position
       assert.equal(second, 42);
     });
     it('object and array', () => {
-      const {z:[x]} = {z: [42]};
+      const {z:[x]} = {z: [ 42]}; //// made z an array of one value
       assert.equal(x, 42);
     });
     it('array and object', () => {
-      const [,{lang}] = [null, {lang: 'ES6'}];
+      const [,[{lang}]] = [null, [{env: 'browser', lang: 'ES6'}]]; /// matched layers of const
       assert.equal(lang, 'ES6');
     });
   });
   describe('interesting', () => {
     it('missing refs become undefined', () => {
-      const {z} = {z};
+      const {z} = {x: 1, y: 2}; /// removed z from object
       assert.equal(z, void 0);
     });
     it('destructure from builtins (string)', () => {
-      const {substr} = String.prototype;
+      const {substr} = String(1);  /// made 1 into a prototype string
       assert.equal(substr, String.prototype.substr);
     });
   });
