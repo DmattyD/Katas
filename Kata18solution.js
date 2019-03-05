6491083b202dbded987506cd2a532feb68864f81
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-03-05 14:35:36 -0700

    Update Kata18solution.js
    added descriptions to solutions

--- Kata18solution.js
+++ Kata18solution.js
@@ -3,29 +3,30 @@
 
 describe('Rest parameters in functions', () => {
   it('must be the last parameter', () => {
-    const fn = (...rest) => {
+    const fn = (...rest ) => { //// removed very last and made ...rest the only parameter, which calls on every other value in the function
       assert.deepEqual([1, 2], rest);
     };
     fn(1, 2);
   });
   it('can be used to get all other parameters', () => {
-    const fn = (firstParam, secondParam, ...rest) => {
+    const fn = (firstParam, secondParam, ...rest) => { /// added ... in front of rest
       assert.deepEqual([3,4], rest);
     };
     fn(null, 2, 3, 4);
   });
   it('makes `arguments` obsolete', () => {
-    const fn = (...args) => {
+    const fn = (...args) => { //// added ...args to the fn =()
       assert.deepEqual([42, 'twenty three', 'win'], args);
     };
     fn(42, 'twenty three', 'win');
   });
   it('eliminate `arguments`!!!', () => {
-    const fn = (...args) => args;
+    const fn = (...args) => args; /// shortened result of function to args, added ...args to function params
     const [firstArg, ...rest] = fn(1, 2, 3);
     assert.deepEqual([2, 3], rest);
   });
 });
+
 
 /////BROKEN CODE BELOW////
 // 18: rest - as-parameter
