af7333f471573939f533ba1764bf88f4c6278694
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-03-05 14:09:51 -0700

    Update Kata17solution.js
    added solution descriptions to solutions for review

--- Kata17solution.js
+++ Kata17solution.js
@@ -4,22 +4,23 @@
 
 describe('Unicode in strings', () => {
   it('are prefixed with `\\u` (one backslash and u)', () => {
-    const nuclear = `\u2622`;
+    const nuclear = `\u2622`; //// added `\u to beginning of 2622 and closed off with `
     assert.equal(nuclear, '☢');
   });
   it('value is 4 bytes/digits', () => {
-    const nuclear = '\u2622';
+    const nuclear = '\u2622';  //// removed extra '2' from end of unicode string
     assert.equal(`no more ${nuclear}`, 'no more ☢');
   });
   it('even "normal" character`s values can be written as hexadecimal unicode', () => {
-    const nuclear = `\u006E\u006F more \u2622`;
+    const nuclear = `\u006e\u006f more \u2622`; /// changed `\u006A to u0063 and u006b to u006f`
     assert.equal(nuclear, 'no more ☢');
   });
   it('curly braces may surround the value', () => {
-    const nuclear = `\u006E\u006F more \u2622`;
+    const nuclear = `\u006E\u006F more \u2622`; //// removed u0063 from {} and shortened both to proper length of 4
     assert.equal(nuclear, 'no more ☢');
   });
 });
+
 
 /// BROKEN CODE BELOW
 
