a5af006c9cc9a5764cf3d00c06051b7cdff38529
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-03-05 13:54:05 -0700

    Update Kata16Solution.js
    updated solutions

--- Kata16Solution.js
+++ Kata16Solution.js
@@ -5,32 +5,33 @@
 describe('Object literal properties may be computed values', () => {
   it('a computed property `x` needs to be surrounded by `[]`', () => {
     const propertyName = 'x';
-    const obj = {x: 1};
+    const obj = {[propertyName]: 1}; /// placed propertyName into array to assign x to obj.x
     assert.equal(obj.x, 1);
   });
   it('can also get a function assigned', () => {
     const key = 'func';
-    const obj = {[key]() {return 'seven'}};
+    const obj = {[key]() {return 'seven'}}; // placed () after [key] to have it act as a function
     assert.equal(obj.func(), 'seven');
   });
   it('the key may also be the result of a function call', () => {
-    const getName = (obj) => 'propertyName';
-    const obj = getName(); {return 'seven'};
+    const getName = () => 'propertyName';
+    const obj = getName(); {return 'seven'} /// pulled getName() from {[getName]} to make it initiate the function
     assert.equal(obj.propertyName(), 'seven');
   });
   it('the key can also be constructed by an expression', () => {
-    const what = 'tyName';
+    const what = 'tyName'; /// changed Key to 'tyName' so it would add to end of proper in line
     const obj = {['proper' + what]: null};
     assert('propertyName' in obj);
   });
   it('accessor keys can be computed names too', () => {
     const obj = {
-       key : 1,
+      get ['key']() {return 1}, //// changed set to get, removed _ from ()
       
     };
     assert.equal(obj.key, 1);
   });
 });
+
 
 //// BROKEN CODE BELOW
 
